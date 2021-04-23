940afc1abb467d4c8874c21307b9f20a1bb4c1f0
章海洋 <403724532@qq.com>
2021-04-23 13:41:26 +0800

    feat: 修改安卓

--- packages/hd/src/template.tsx
+++ packages/hd/src/template.tsx
@@ -1,14 +1,14 @@
-import vw from 'umi-hd/lib/vw';
+import vw from 'umi-hd/lib/vw-no-android-hd';
 import flex from 'umi-hd/lib/flex';
 
 // Fix document undefined when ssr. #2571
 if (typeof document !== 'undefined') {
-  if (document.documentElement.clientWidth >= 750) {
-    vw(100, 750);
-  } else {
-    flex(100, window.alitaFontScale || 1);
-  }
-
+  // if (document.documentElement.clientWidth >= 750) {
+  //   vw(100, 750);
+  // } else {
+  //   flex(100, window.alitaFontScale || 1);
+  // }
+  vw(100, 750);
   // hd solution for antd-mobile@2
   // ref: https://mobile.ant.design/docs/react/upgrade-notes-cn#%E9%AB%98%E6%B8%85%E6%96%B9%E6%A1%88
   document.documentElement.setAttribute('data-scale', true);
